a85240542979391e9f0d3bcc8ba241af1d51ec9f
ATRI2107 <atrichaturvedi@gmail.com>
2020-01-28 03:05:38 +0530

    First commit for HashMap in Java

---
+++ HASHMAP/hashmap.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class hashmap
+{
+    static void CharFreq(String s)
+    {
+        HashMap<Character,Integer> hm=new HashMap<Character,Integer>();
+        char a[]=s.toCharArray();
+        for(char c:a)
+        {
+            if(hm.containsKey(c))
+            hm.put(c,hm.get(c)+1);
+            else
+            hm.put(c,1);
+        }
+        for(Map.Entry m:hm.entrySet())
+        {
+            System.out.print(m.getKey()+" "+m.getValue());
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        String x;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a String");
+        x=sc.next();
+        CharFreq(x);
+    }
+}
