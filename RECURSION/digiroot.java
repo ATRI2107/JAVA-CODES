a419a9911e6a110bc0776d56af6107b0edc7740b
ATRI2107 <atrichaturvedi@gmail.com>
2020-02-16 02:51:04 +0530

    Code written

---
+++ RECURSION/digiroot.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class digiroot
+{
+    static int droot(int n)
+    {
+        if(n<10) return n;
+        return droot(n-9);
+    }
+    public static void main(String[] args) {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number");
+        n=sc.nextInt();
+        System.out.println("Digital Root of "+n+" is:"+droot(n));
+    }
+}
