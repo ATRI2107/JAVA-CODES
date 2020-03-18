fa380f750b99eeb2cf6c615d5ce45d0d24a9fb95
Atri Chaturvedi <atrichaturvedi@gmail.com>
2020-03-18 12:36:18 +0530

    Created the program

---
+++ STACK/Smallest Number k/abc.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class abc
+{
+    static long smallest_numberk(int n)
+    {
+        if(n>=0 && n<=9) return n;
+        Stack<Integer> st=new Stack<>(); //This stack stores the digits
+        for(int i=9;n>1 && i>1;i--)
+        {
+            while(n%i==0)
+            {
+                st.push(i);
+                n/=i;
+            }
+            if(n!=1) return -1;
+
+        }
+        long prod=0;
+        while(!st.empty())
+        {
+            prod=prod*10+st.peek();
+            st.pop();
+        }
+        return prod;
+    }
+    public static void main(String[] args) {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number n");
+        n=sc.nextInt();
+        System.out.println(smallest_numberk(n));
+        sc.close();
+    }
+}
