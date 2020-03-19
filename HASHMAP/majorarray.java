bfdb25986d56fd8a23b1f8f9bf40c3b03aa296c7
Atri Chaturvedi <atrichaturvedi@gmail.com>
2020-03-19 10:46:39 +0530

    Code written

--- HASHMAP/majorarray.java
+++ HASHMAP/majorarray.java
@@ -3,6 +3,28 @@ class majorarray
 {
     public static void main(String[] args) {
         int n;
-        Scanner
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the array");
+        n=sc.nextInt();
+        int arr[]=new int[n];
+        System.out.println("Enter the elements of the array");
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        for(int i:arr)
+        {
+            if(hm.containsKey(i))
+            {
+                hm.put(i,hm.get(i)+1);
+            }
+            else
+            {
+                hm.put(i,1);
+            }
+        }
+        System.out.println(Collections.max(hm.entrySet(),(entry1,entry2)->entry1.getValue()-entry2.getValue()).getKey());
+
     }
 }
