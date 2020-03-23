1648b2c80ce89f17eb840e374d454feca30acec6
Atri Chaturvedi <atrichaturvedi@gmail.com>
2020-03-23 16:17:26 +0530

    Code written

---
+++ SUBARRAY-WITH-A-GIVEN-SUM/abc.java
@@ -0,0 +1,42 @@
+import java.util.*;
+class abc
+{
+    static void subarraysum(int arr[],int n,int s)
+    {
+        int i=0,start=0,end=0,curr_sum=0;
+        while(i<n)
+        {
+            if(curr_sum<s)
+            {
+                curr_sum+=arr[i];
+                end=i;
+                i++;
+            }
+            if(curr_sum>s)
+            {
+                curr_sum-=arr[start++];
+            }
+            if(curr_sum==s)
+            {
+                System.out.println((start+1)+" "+(end+1));
+                break;
+            }
+        }
+        if(curr_sum!=s)
+        {
+            System.out.println(-1);
+        }
+    }
+    public static void main(String[] args) {
+        int s,n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        s=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        subarraysum(arr,n,s);
+    }
+}
