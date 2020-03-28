06c154cccccf8ff3a24bf30eeb2b6f16d61561d6
Atri Chaturvedi <atrichaturvedi@gmail.com>
2020-03-28 19:39:30 +0530

    Code Done

---
+++ AMAZON CODES/Replacing 0's with 5's/abc.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class abc
+{
+    public static void main(String[] args) {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int c=0,s=0;
+        for(int i=n;i>0;i/=10)
+        {
+            if(i%10==0)
+            {
+                s+=5*(int)(Math.pow(10,c));
+            }
+            c++;
+        }
+        System.out.println(s+n);
+    }
+}
