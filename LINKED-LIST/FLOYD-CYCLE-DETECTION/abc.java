a09f10695775fdeec50e39362cf664e3fb359a83
Atri Chaturvedi <atrichaturvedi@gmail.com>
2020-04-07 23:34:21 +0530

    Code tested with a loop

--- LINKED-LIST/FLOYD-CYCLE-DETECTION/abc.java
+++ LINKED-LIST/FLOYD-CYCLE-DETECTION/abc.java
@@ -22,3 +22,17 @@ class List
         return false;
     }
 }
+class abc
+{
+    public static void main(String[] args) {
+        List list=new List();
+        list.head = new Node(50); 
+        list.head.next = new Node(20); 
+        list.head.next.next = new Node(15); 
+        list.head.next.next.next = new Node(4); 
+        list.head.next.next.next.next = new Node(10); 
+        // Creating a loop for testing 
+        list.head.next.next.next.next.next = list.head.next.next;
+        System.out.println(list.cycleDetect()); 
+    }
+}
