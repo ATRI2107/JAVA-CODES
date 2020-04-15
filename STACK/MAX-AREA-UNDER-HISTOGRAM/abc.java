a57cd078c466465031886d37cd7a0823c37a1fed
Atri Chaturvedi <atrichaturvedi@gmail.com>
2020-04-15 19:50:47 +0530

    Main code

---
+++ STACK/MAX-AREA-UNDER-HISTOGRAM/abc.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class abc {
+public static void main(String[] args) {
+    int n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        Stack<Integer> st=new Stack<>();
+        int res=0;
+        for(int i=0;i<n;i++)
+        {
+            while(!st.isEmpty() && a[st.peek()]>=a[i])
+            {
+                int x=st.pop();
+                int area=a[x]*(st.isEmpty()?1:(i-st.peek()-1)); 
+                res=Math.max(area,res);
+            }
+            st.push(i);
+        }
+        while(!st.isEmpty())
+        {
+            int x=st.pop();
+            int area=a[x]*(st.isEmpty()?n:n-st.peek()-1);
+            res=Math.max(area,res);
+        }
+        System.out.println(res);
+}
+}
