11055a5ce076b51a17694bebe1fa324aedc282c1
Atri Chaturvedi <atrichaturvedi@gmail.com>
2020-04-25 02:38:24 +0530

    Added Deserialization

--- TREES/SERIALIZE-DESRIALIZE-A-TREE/abc.java
+++ TREES/SERIALIZE-DESRIALIZE-A-TREE/abc.java
@@ -14,13 +14,34 @@ class Tree
     {
         if(root==null)
         {
-            al.add(-1);
+            al.add(-1); //We are using -1 to represent the null value
             return;
         }
         al.add(root.data);
         serialize(root.left, al);
         serialize(root.right, al);
     }
+    void inorder(Node root) // To confirm the deserialize logic
+    {
+        if(root!=null)
+        {
+            inorder(root.left);
+            System.out.print(root.data+" ");
+            inorder(root.right);
+        }
+    }
+    int index=0;
+    Node desrialize(ArrayList<Integer> al)
+    {
+        if(index==al.size()) return null;
+        int val=al.get(index++);
+        if(val==-1) return null;
+        Node root=new Node(val);
+        root.left=desrialize(al);
+        root.right=desrialize(al);
+        return root;
+    }
+
 }
 class abc
 {
