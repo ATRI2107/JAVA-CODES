03ce621fc8a913bbb34d88c17698debc8fdb243d
Atri Chaturvedi <atrichaturvedi@gmail.com>
2020-04-27 12:54:42 +0530

    Added the leaf count function

--- TREES/SIZE-HEIGHT-MAXIMUM-OF-A-TREE/abc.java
+++ TREES/SIZE-HEIGHT-MAXIMUM-OF-A-TREE/abc.java
@@ -38,12 +38,22 @@ class Tree
             printKDist(root.right, k-1);
         }
     }
+    int c=0;
+    void countLeafNodes(Node root)
+    {
+        if(root==null) return;
+        if(root.left==null && root.right==null) c++;
+        countLeafNodes(root.left);
+        countLeafNodes(root.right);
+    }
 }
 class abc
 {
     public static void main(String[] args) {
         Node root=new Node(10);
         root.left=new Node(20);
+        root.left.left=new Node(60);
+        root.left.right=new Node(70);
         root.right=new Node(30);
         root.right.left=new Node(40);
         root.right.right=new Node(50);
@@ -54,5 +64,7 @@ class abc
         System.out.print("Nodes at a distance k: ");
         obj.printKDist(root, 2); //here k is 2
         System.out.println();
+        obj.countLeafNodes(root);
+        System.out.println("Number of Leaf Nodes="+obj.c);
     }
 }
