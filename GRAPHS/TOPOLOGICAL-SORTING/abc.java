02c1c78b0f90d1a4ddd22ab17bb4316f921e72eb
Atri Chaturvedi <atrichaturvedi@gmail.com>
2020-05-20 18:24:27 +0530

    Main code

---
+++ GRAPHS/TOPOLOGICAL-SORTING/abc.java
@@ -0,0 +1,63 @@
+import java.util.*;
+class abc
+{
+    static void topologicalsort(ArrayList<ArrayList<Integer>> list,int N)
+    {
+        int in[]=new int[N];
+        Arrays.fill(in,0);
+        for(ArrayList<Integer> x:list)
+        {
+            for(int i:x)
+            {
+                in[i]++;
+            }
+        }
+        boolean vis[]=new boolean[N];
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<N;i++)
+        {
+            if(in[i]==0)
+            {
+                vis[i]=true;
+                q.add(i);
+            }
+        }
+        ArrayList<Integer> res=new ArrayList<>();
+        while(!q.isEmpty())
+        {
+            int curr=q.poll();
+            res.add(curr);
+            for(int v:list.get(curr))
+            {
+                in[v]--;
+                if(in[v]==0 && !vis[v])
+                {
+                    vis[v]=true;
+                    q.add(v);
+                }
+            }
+        }
+        for(int i:res)
+        {
+            System.out.print(i+" ");
+        }
+        System.out.println();
+        
+    }
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> al=new ArrayList<>();
+        Scanner sc=new Scanner(System.in);
+        int v=sc.nextInt();
+        for(int i=0;i<v;i++)
+        {
+            ArrayList<Integer> temp=new ArrayList<>();
+            int n=sc.nextInt();
+            for(int j=0;j<n;j++)
+            {
+                temp.add(sc.nextInt());
+            }
+            al.add(temp);
+        }
+        topologicalsort(al, v);
+    }
+}
