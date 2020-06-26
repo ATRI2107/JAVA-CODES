b580b32c62b193aad935fd1985aa29cfbc22ccc7
ATRI2107 <atrichaturvedi@gmail.com>
2020-06-26 15:07:11 +0530

    Main code done

---
+++ ARRAYS/3-SUM/abc.java
@@ -0,0 +1,38 @@
+import java.util.*;
+class abc
+{
+    public static void main(String[] args) {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int nums[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            nums[i]=sc.nextInt();
+        }
+        Arrays.sort(nums);
+        List<List<Integer>> res=new ArrayList<>();
+        for(int i=0;i<nums.length-2;i++)
+        {
+            if(i==0 || nums[i]!=nums[i-1])
+            {
+                int l=i+1;
+                int h=nums.length-1;
+                while(l<h)
+                {
+                    if(nums[i]+nums[l]+nums[h]==0)
+                    {
+                        res.add(Arrays.asList(nums[i],nums[l],nums[h]));
+                        while(l<h && nums[l]==nums[l+1]) l++;
+                        while(l<h && nums[h]==nums[h-1]) h--;
+                        l++;
+                        h--;
+                    }
+                    else if(nums[i]+nums[l]+nums[h]>0) h--;
+                    else l++;
+                }
+            }
+        }
+        System.out.println(res);
+    }
+}
