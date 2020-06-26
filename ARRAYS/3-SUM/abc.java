c1d9d9206c691dd541a4b7fd2f0d7c9b90233464
ATRI2107 <atrichaturvedi@gmail.com>
2020-06-26 15:08:29 +0530

    Comments added

--- ARRAYS/3-SUM/abc.java
+++ ARRAYS/3-SUM/abc.java
@@ -14,7 +14,7 @@ class abc
         List<List<Integer>> res=new ArrayList<>();
         for(int i=0;i<nums.length-2;i++)
         {
-            if(i==0 || nums[i]!=nums[i-1])
+            if(i==0 || nums[i]!=nums[i-1]) // To check for duplicates if there then skip
             {
                 int l=i+1;
                 int h=nums.length-1;
@@ -23,8 +23,8 @@ class abc
                     if(nums[i]+nums[l]+nums[h]==0)
                     {
                         res.add(Arrays.asList(nums[i],nums[l],nums[h]));
-                        while(l<h && nums[l]==nums[l+1]) l++;
-                        while(l<h && nums[h]==nums[h-1]) h--;
+                        while(l<h && nums[l]==nums[l+1]) l++; // To check for duplicates
+                        while(l<h && nums[h]==nums[h-1]) h--; // to check for duplicates
                         l++;
                         h--;
                     }
