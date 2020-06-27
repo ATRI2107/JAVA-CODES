ec1615ed5f04597951e1c083341bbaa9ce3093c4
ATRI2107 <atrichaturvedi@gmail.com>
2020-06-27 20:48:46 +0530

    Added Bianry search method

--- DYNAMIC-PROGRAMMING/LONGEST-INCREASING-SUBSEQUENCE/abc.java
+++ DYNAMIC-PROGRAMMING/LONGEST-INCREASING-SUBSEQUENCE/abc.java
@@ -1,4 +1,42 @@
 import java.util.*;
+
+class BinarySearchMethod // Method to sove it in O(nlogn)
+{
+    int ceil(int[] tail,int b,int e,int x)
+    {
+        int l=b;
+        int r=e;
+        while(l<r)
+        {
+            int m=l+(r-l)/2;
+            if(x<=tail[m])
+            r=m;
+            else
+            l=m+1;
+        }
+        return r;
+    }
+    int lis(int[] a,int n)
+    {
+        int tail[]=new int[n];
+        tail[0]=a[0];
+        int len=1;
+        for(int i=1;i<n;i++)
+        {
+            if(a[i]>tail[len-1])
+            {
+                tail[len]=a[i];
+                len++;
+            }
+            else
+            {
+                int c=ceil(tail, 0, len-1, a[i]);
+                tail[c]=a[i];
+            }
+        }
+        return len;
+    }
+}
 class abc
 {
     static int lis(int[] a,int n) // Dynamic Programming solution
@@ -31,6 +69,7 @@ class abc
             a[i]=sc.nextInt();
         }
         System.out.println(lis(a, n));
+        System.out.println(new BinarySearchMethod().lis(a, n));
         sc.close();
     }
 }
