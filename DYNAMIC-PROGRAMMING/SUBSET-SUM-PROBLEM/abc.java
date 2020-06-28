47332d4f8b7e4348a86145c45c2e762c2d66b5f3
ATRI2107 <atrichaturvedi@gmail.com>
2020-06-28 18:03:50 +0530

    Main code

---
+++ DYNAMIC-PROGRAMMING/SUBSET-SUM-PROBLEM/abc.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class abc
+{
+    public static void main(String[] args) {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        int sum=sc.nextInt();
+        int dp[][]=new int[n+1][sum+1];
+        for(int i=0;i<=n;i++)
+        {
+            dp[i][0]=1;
+        }
+        for(int i=1;i<=sum;i++)
+        {
+            dp[0][i]=0;
+        }
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=sum;j++)
+            {
+                dp[i][j]=dp[i-1][j];
+                if(j>=a[i-1])
+                dp[i][j]+=dp[i][j-a[i-1]];
+            }
+        }
+        System.out.println(dp[n][sum]);
+    }
+}
