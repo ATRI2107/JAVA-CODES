a62f762d6ec50580264c6471adf34f6a899ab392
ATRI2107 <atrichaturvedi@gmail.com>
2020-07-26 23:38:57 +0530

    No change

--- GRAPHS/CATS-RESTRAUNTS/abc.java
+++ GRAPHS/CATS-RESTRAUNTS/abc.java
@@ -1,84 +1,44 @@
 import java.util.*;
 public class abc
 {
-    ArrayList<ArrayList<Integer>> paths=new ArrayList<>();
-    void dfs(ArrayList<Integer> adj[],int s,LinkedList<Integer> path)
+    int res=0;
+    void dfs(int par,int s,int count,ArrayList<Integer> adj[],int cats[],int m,boolean vis[])
     {
-        path.add(s);
-        
-        if(adj[s].size()==0)
+        vis[s]=true;
+        if(cats[s]==1) count++;
+        else count=0;
+        if(count>m) return;
+        if(adj[s].size()==1 && s!=1) res++;
+        for(int u:adj[s])
         {
-            /*for(int i:path)
-            {
-                System.out.print(i+" ");
-            }
-            System.out.println();*/
-            ArrayList<Integer> temp=new ArrayList<>();
-            temp.addAll(path);
-            paths.add(temp);
-            path.removeLast();
-            return;
+            if(!vis[u]) dfs(s,u, count, adj, cats,m,vis);
         }
-        for(int u: adj[s])
-        {
-            dfs(adj, u, path);           
-        }
-        path.removeLast();  
     }
-    int numberOfRestraunts(int[] conseq,int m)
+    int dfsutil(ArrayList<Integer> adj[],int cats[],int m,boolean vis[])
     {
-        
-        int able_to_visit=0;
-        for(ArrayList<Integer> p:paths)
-        {
-            int count=0,res=Integer.MIN_VALUE;
-            for(int i=1;i<p.size();i++)
-            {
-                if(conseq[p.get(i)]==1 && conseq[p.get(i-1)]==1)
-                {
-                    count++;
-                    res=Math.max(count+1,res);
-                    // System.out.println(res);
-                }
-                else
-                {
-                    count=0;
-                }
-            }
-            if(res<=m) able_to_visit++;
-        }
-        return able_to_visit;
+        dfs(0, 1, 0, adj, cats, m, vis);
+        return res;
     }
     public static void main(String[] args) {
         int n,m;
         Scanner sc=new Scanner(System.in);
         n=sc.nextInt();
         m=sc.nextInt();
-        int conseq[]=new int[n+1];
-        conseq[0]=0;
+        int cats[]=new int[n+1];
         for(int i=1;i<=n;i++)
         {
-            conseq[i]=sc.nextInt();
+            cats[i]=sc.nextInt();
         }
         ArrayList<Integer> adj[]=new ArrayList[n+1];
-        for(int i=0;i<=n;i++)
-        {
-            adj[i]=new ArrayList<>();
-        }
-        for(int i=0;i<n-1;i++)
+        Arrays.fill(adj,new ArrayList<Integer>());
+        for(int i=1;i<n;i++)
         {
             int x=sc.nextInt();
             int y=sc.nextInt();
-            if(y<x)
-            adj[y].add(x);
-            else
             adj[x].add(y);
-            
+            adj[y].add(x);
         }
-        
-        abc obj=new abc();
-        obj.dfs(adj, 1, new LinkedList<>());
-        System.out.println(obj.numberOfRestraunts(conseq,m));
-        sc.close();
+        boolean vis[]=new boolean[n+1];
+        System.out.println(new abc().dfsutil(adj, cats, m, vis));  
     }
 }
